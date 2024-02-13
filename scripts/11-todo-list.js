c3ad699d8469520d2a2cadec0bfd3be6516716b2
Luisa Perez <luisa.perezw3@gmail.com>
2024-02-13 17:05:17 +0000

    adding date for todo list, style for list in grid and style for input boxes

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,4 +1,13 @@
-const todoList = [];
+const todoList = [
+  {
+    name: "make dinner",
+    dueDate: "2022-12-22",
+  },
+  {
+    name: "make bed",
+    dueDate: "2022-12-22",
+  },
+];
 
 renderTodoList();
 
@@ -6,20 +15,26 @@ function renderTodoList() {
   let todoListHTML = "";
   // todoList.length is the shortcut of todo.List.lenght -1, which will stop the loop at the last element of the array. Also i is the index
   for (let i = 0; i < todoList.length; i++) {
-    const element = todoList[i];
-    const html = `<p>
-      ${element}
+    const todoObject = todoList[i];
+    // const name = todoObject.name;
+    // const dueDate = todoObject.dueDate;
+    // the below is a shorcut of the two above, instead of writing two different variables
+    const { name, dueDate } = todoObject;
+
+    const html = `
+      <div>${name} </div>
+      <div>${dueDate} </div>
       <button onClick="
         todoList.splice(${i},1)
         renderTodoList();
       ">Delete</button>
-    </p>`;
+      `;
     todoListHTML += html;
   }
 
   // splice does take 2 values, first the value we want to delete and second how many in our case 1
 
-  console.log(todoListHTML);
+  // console.log(todoListHTML);
 
   document.querySelector(".js-todo-list").innerHTML = todoListHTML;
 }
@@ -27,7 +42,15 @@ function renderTodoList() {
 function addTodo() {
   const inputElement = document.querySelector(".js-name-input");
   const name = inputElement.value;
-  todoList.push(name);
+
+  const dateInputEelement = document.querySelector(".js-due-date-input");
+  const dueDate = dateInputEelement.value;
+  todoList.push({
+    // name: name,
+    // dueDate: dueDate, when both are the same we can just use name and due Date to re factor, shorhadn property sytax
+    name,
+    dueDate,
+  });
   inputElement.value = "";
   renderTodoList();
 }
